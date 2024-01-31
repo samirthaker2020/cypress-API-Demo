559b84cf5a76ab29fae7a1e682a0ede5bb0b473a
samirthaker2020 <samir2009thaker@gmail.com>
2024-01-30 22:36:06 -0400

    test: add post request test

--- cypress/e2e/API Testing/httpRequest.cy.js
+++ cypress/e2e/API Testing/httpRequest.cy.js
@@ -3,4 +3,14 @@ describe("HTTP Request", ()=>{
     it("GET Call",()=>{
         cy.request('GET', 'https://jsonplaceholder.typicode.com/posts/1').its('status').should('equal',200)
     })
+    it('POST request', ()=>{
+        cy.request({
+            method:'post',
+            url:'https://jsonplaceholder.typicode.com/posts/',
+            body:{
+                title:'test',
+                body:'this is a test'
+            }
+        }).its('status').should('equal',201)
+    })
 })
