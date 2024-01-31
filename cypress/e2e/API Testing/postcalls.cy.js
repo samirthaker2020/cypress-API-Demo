1e44c9253217f05ae53ae5bda24aecc19748de26
samirthaker2020 <samir2009thaker@gmail.com>
2024-01-30 23:10:07 -0400

    test: approach 2 dynamic generate payload

--- cypress/e2e/API Testing/postcalls.cy.js
+++ cypress/e2e/API Testing/postcalls.cy.js
@@ -2,7 +2,7 @@ describe('api testing', ()=>{
     it('Apporach 1- hard coded json object',()=>{
         const req= {
                 tourist_name: "Lindsay",
-                tourist_email: "cole.dbrddke@gmail.com",
+                tourist_email: "cole.dbrd8dke@gmail.com",
                 tourist_location: "Priscillaton"
         }
         cy.request({
@@ -11,6 +11,20 @@ describe('api testing', ()=>{
             body: req}).then((res) =>{
                 expect(res.status).to.eq(201)
                 expect(res.body.tourist_name).to.eq('Lindsay')
+            })
+    })
+    it('Apporach 2- dynamically generating json object',()=>{
+        const req= {
+                tourist_name: Math.random().toString(5).substring(2),
+                tourist_email: Math.random().toString(5).substring(2)+"@gmail.com",
+                tourist_location: "Priscillaton"
+        }
+        cy.request({
+            method:'POST',
+            url:'http://restapi.adequateshop.com/api/Tourist',
+            body: req}).then((res) =>{
+                expect(res.status).to.eq(201)
+                expect(res.body.tourist_name).to.eq(req.tourist_name)
             })
     })
 })
